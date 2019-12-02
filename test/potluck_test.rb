535a81f2fcac35512d3787c5acd02738ac8e1170
Ben Fox <benfox1216@gmail.com>
2019-12-02 10:22:45 -0700

    Update dish class & test, add potluck class & test

---
+++ test/potluck_test.rb
@@ -0,0 +1,14 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/dish'
+require './lib/potluck'
+
+class PotluckTest < Minitest::Test
+  def test_it_exists
+
+  end
+
+  def test_it_has_a_date
+
+  end
+end
